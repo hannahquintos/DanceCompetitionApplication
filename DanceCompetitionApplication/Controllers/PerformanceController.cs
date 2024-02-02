352e354d84a02fd2b1f983ab27aafe39cfbf55af
hannahquintos <hannah@quintos.com>
2024-02-01 20:35:02 -0500

    Built update functionality for performances

--- DanceCompetitionApplication/Controllers/PerformanceController.cs
+++ DanceCompetitionApplication/Controllers/PerformanceController.cs
@@ -101,18 +101,44 @@ namespace DanceCompetitionApplication.Controllers
         // GET: Performance/Edit/5
         public ActionResult Edit(int id)
         {
-            return View();
+            // get the performance information
+
+            string url = "findperformance/" + id;
+            HttpResponseMessage response = client.GetAsync(url).Result;
+
+            PerformanceDto selectedperformance = response.Content.ReadAsAsync<PerformanceDto>().Result;
+
+            return View(selectedperformance);
         }
 
-        // POST: Performance/Edit/5
+        // POST: Performance/Update/5
         [HttpPost]
-        public ActionResult Edit(int id, FormCollection collection)
+        public ActionResult Update(int id, Performance performance)
         {
             try
             {
-                // TODO: Add update logic here
+                /*Debug.WriteLine("The new performance info is:");
+                Debug.WriteLine(performance.RoutineName);
+                Debug.WriteLine(performance.PerformanceTime);
+                Debug.WriteLine(performance.Studio);
+                Debug.WriteLine(performance.CategoryId);*/
 
-                return RedirectToAction("Index");
+                //send the request to the API
+                string url = "updateperformance/" + id;
+
+                // serialize into JSON
+                string jsonpayload = jss.Serialize(performance);
+                /*Debug.WriteLine(jsonpayload);*/
+
+                HttpContent content = new StringContent(jsonpayload);
+                content.Headers.ContentType.MediaType = "application/json";
+
+                // POST: api/PerformanceData/UpdatePerformance/{id}
+                // Header : Content-Type: application/json
+
+                HttpResponseMessage response = client.PostAsync(url, content).Result;
+
+                return RedirectToAction("Details/"+id);
             }
             catch
             {
